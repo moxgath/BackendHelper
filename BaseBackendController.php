1036efdcf91d2e3e1aa2c1769c4499c0b4b0abe4
JJELEMENT <jjelementza@gmail.com>
2016-11-29 19:46:33 +0700

    fix_renderEdit

--- BaseBackendController.php
+++ BaseBackendController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Game;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Moxga\BackendHelper\BackendHelper;
-use DateTime;
 
 class BaseBackendController extends Controller 
 {
@@ -18,8 +19,34 @@ class BaseBackendController extends Controller
 	public function __construct($title = 'Shosha Backend', $model = null)
 	{
 		$this->backendHelper = new BackendHelper($title, $model);
-		$this->backendHelper->addMenu('Home', route('backend.home'), 'fa fa-dashboard');
+		$this->backendHelper->addMenu('Home', action('Backend\HomeController@index'), 'fa fa-dashboard');
 
+		$newsMenu = $this->backendHelper->addMenu('News', "#", 'fa fa-newspaper-o');
+		$newsMenu->addSubMenu('Topic', action('Backend\NewsController@index'), 'fa fa-list');
+		$newsMenu->addSubMenu('Comment', action('Backend\NewsCommentController@index'), 'fa fa-comments');
+
+		$eventMenu = $this->backendHelper->addMenu('Event', "#", 'fa fa-star');
+		$eventMenu->addSubMenu('Topic', action('Backend\EventController@index'), 'fa fa-list');
+		$eventMenu->addSubMenu('Comment', action('Backend\EventCommentController@index'), 'fa fa-comments');
+
+		$webboardMenu = $this->backendHelper->addMenu('Webboard', "#", 'fa fa-list-alt');
+		$webboardMenu->addSubMenu('Topic', action('Backend\WebboardController@index'), 'fa fa-list');
+		$webboardMenu->addSubMenu('Category', action('Backend\WebboardCategoryController@index'), 'fa fa-list');
+		$webboardMenu->addSubMenu('Comment', action('Backend\WebboardCommentController@index'), 'fa fa-comments');
+
+		$userMenu = $this->backendHelper->addMenu('User', "#", 'fa fa-user');
+		$userMenu->addSubMenu('List', action('Backend\UserController@index'), 'fa fa-list');
+		$userMenu->addSubMenu('Comment', action('Backend\UserCommentController@index'), 'fa fa-comments');
+
+		$this->backendHelper->addMenu('Team', action('Backend\TeamController@index'), 'fa fa-users');
+
+		$gameMenu = $this->backendHelper->addMenu('Game', '#', 'fa fa-gamepad');
+		$gameList = Game::get();
+		foreach($gameList as $game) {
+			$subGameMenu = $gameMenu->addSubMenu($game->name, '#', 'fa fa-folder');
+			$subGameMenu->addSubMenu('Match', action('Backend\MatchController@index', ['game_id' => $game->id]), 'fa fa-list');
+			$subGameMenu->addSubMenu('Match Result', action('Backend\MatchResultController@index', ['game_id' => $game->id]), 'fa fa-list');
+		}
 
 		$this->model     = $this->backendHelper->getModel();
 		$this->baseRoute = $this->backendHelper->getBaseRoute();
