ed1a2b97d67af6b4d8795d59970a311edfc5391f
JJELEMENT <jjelementza@gmail.com>
2016-11-29 20:07:09 +0700

    editFile

--- resources/views/panelBody.blade.php
+++ resources/views/panelBody.blade.php
@@ -8,6 +8,9 @@
 		<div class="form-group">
 			{!! Form::label($field->getName(), $field->getLabel(), ['class' => 'control-label col-sm-2']) !!}
 			<div class="col-sm-7">
+				@if($field->getType() == 'file' && $backendHelper->getEditItem())
+					<img src="{{ route('imagecache', ['0x300', $backendHelper->getEditItem()->{$field->getName()}]) }}" alt="">
+				@endif
 				{!! $field->render() !!}
 			</div>
 		</div>
